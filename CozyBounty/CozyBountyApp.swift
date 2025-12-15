94400148baccdf758fa6a48034a096df1bd7cf12
milxuan <wangqixuan24@163.com>
2025-12-15 19:43:47 +0800

    Initial Commit

---
+++ CozyBounty/CozyBountyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CozyBountyApp.swift
+//  CozyBounty
+//
+//  Created by liepin on 2025/12/15.
+//
+
+import SwiftUI
+
+@main
+struct CozyBountyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
